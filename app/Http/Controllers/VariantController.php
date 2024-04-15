5b23cd55aead29eeba7e55347f526e549f51b212
chisyanaintanhabibah <chisyana.intan@gmail.com>
2024-04-15 23:08:07 +0700

    scnd commit

---
+++ app/Http/Controllers/VariantController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VariantController extends Controller
+{
+    public function index()
+{
+$prods = Product::get();
+return view('product.index', ['list' => $prods]);
+}
+public function create()
+{
+return view('product.form', [
+'title' => 'Tambah',
+'method' => 'POST',
+'action' => 'product'
+]);
+}
+public function store(Request $request)
+{
+$prod = new Product;
+$prod->name = $request->name;
+$prod->price = $request->price;
+$prod->save();
+return redirect('/product')->with('msg', 'Tambah berhasil');
+}
+public function show($id)
+{
+return Product::find($id);
+}
+public function edit($id)
+{
+return view('product.form', [
+'title' => 'Edit',
+'method' => 'PUT',
+'action' => "product/$id",
+'data' => Product::find($id)
+]);
+}
+public function update(Request $request, $id)
+{
+$prod = Product::find($id);
+$prod->name = $request->name;
+$prod->price = $request->price;
+$prod->save();
+return redirect('/product')->with('msg', 'Edit berhasil');
+}
+public function destroy($id)
+{
+Product::destroy($id);
+// atau
+/* $prod = Product::find($id);
+$prod->delete(); */
+return redirect('/product')->with('msg', 'Hapus berhasil');
+}
+}
+     
+
+ 
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/variants', [App\Http\Controllers\VariantController::class]);
+Route:get('/login', function(){
+    if(Auth::check())return redirect('product');
+    return view('login');
+})->name('login')
+Route::resource('product', ProductController::class)->middleware('auth');
